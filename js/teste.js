a16214616d55aafdd045110214f896ac37dec525
Henry Guidelli <71869428+HenryGuidelli@users.noreply.github.com>
2023-02-06 09:51:36 -0300

    Update teste.js

--- js/teste.js
+++ js/teste.js
@@ -1,1 +1,13 @@
-alert("oi doo")
+function createButton()
+{
+     var btn = document.createElement('BUTTON');
+     var lbl = document.createTextNode("CLICK ME");        
+     btn.appendChild(lbl);   
+     btn.onclick = function()
+     {
+        alert("oi do botão")
+     }
+     document.body.appendChild(btn);    
+}
+
+createButton();
